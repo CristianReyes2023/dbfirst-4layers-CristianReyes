7817de71ccac1a0b8e1cf747301f88c6706dae2d
CristianReyes2023 <137292974+CristianReyes2023@users.noreply.github.com>
2023-11-09 11:38:54 -0500

    feat: :sparkles: Injection added in dependency container

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,6 @@
+using System.Reflection;
+using API.Extensions;
+using AspNetCoreRateLimit;
 using Microsoft.EntityFrameworkCore;
 using Persistence.Data;
 
@@ -9,6 +12,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.ConfigureCors();
+builder.Services.ConfigureRateLimiting();
+builder.Services.AddApplicationServices();
+builder.Services.AddAutoMapper(Assembly.GetEntryAssembly());
 
 builder.Services.AddDbContext<ejemplodb4layersContext>(optionsBuilder => // 2611
 {
@@ -42,6 +49,8 @@ using (var scope = app.Services.CreateScope())
         _logger.LogError(ex, "Ocurrio un error durante la migracion");
     }
 }
+app.UseCors("CorsPolicy");
+app.UseIpRateLimiting();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
